79a47f87411c521fa7851dedde533e1b09d87a3f
Sugat Poudel <taguspoudel@gmail.com>
2024-08-04 15:09:44 -0400

    updated logger api to use slog

--- logger.go
+++ logger.go
@@ -1,149 +1,56 @@
 package mango
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"os"
 	"runtime/debug"
-	"strings"
-
-	"github.com/fatih/color"
 )
 
-type LogLevel int8
-
-var (
-	logger *log.Logger = log.New(os.Stdout, "", log.Ltime)
-	lvl    LogLevel    = LogLevelDebug
-)
-
-const (
-	LogLevelDebug LogLevel = iota
-	LogLevelInfo
-	LogLevelWarning
-	LogLevelError
-)
-
-func (l LogLevel) String() string {
-	switch l {
-	case LogLevelError:
-		return color.RedString("[ERROR]")
-	case LogLevelWarning:
-		return color.YellowString("[WARN]")
-	case LogLevelInfo:
-		return color.GreenString("[INFO]")
-	case LogLevelDebug:
-		return "[DEBUG]"
-	default:
-		return ""
+func Init(level slog.Level, prefix string) {
+	h := &LogHandler{
+		prefix: prefix,
+		handler: slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: level,
+		}),
 	}
+	logger := slog.New(h)
+	slog.SetDefault(logger)
 }
 
-// Deprecated: use slog instead
-func Init(level LogLevel, prefix string) {
-	lvl = level
-	lp := ""
-	if prefix != "" {
-		lp = fmt.Sprintf("[%s] ", prefix)
-	}
-	logger = log.New(os.Stdout, lp, log.Ltime)
-}
-
-// Deprecated: use slog instead
-func decorate(l LogLevel, format string) string {
-	if format == "" {
-		return fmt.Sprintf("%v", l)
-	}
-	if !strings.HasSuffix(format, "\n") {
-		format += "\n"
-	}
-	return fmt.Sprintf("%v %s", l, format)
-}
-
-// Deprecated: use slog instead
-func Debug(v ...any) {
-	if lvl > LogLevelDebug {
-		return
-	}
-	args := []any{decorate(LogLevelDebug, "")}
-	args = append(args, v...)
-	logger.Println(args...)
-}
-
-// Deprecated: use slog instead
-func Debugf(format string, v ...any) {
-	if lvl > LogLevelDebug {
-		return
-	}
-	logger.Printf(decorate(LogLevelDebug, format), v...)
-}
-
-// Deprecated: use slog instead
-func Warning(v ...any) {
-	if lvl > LogLevelWarning {
-		return
-	}
-	args := []any{decorate(LogLevelWarning, "")}
-	args = append(args, v...)
-	logger.Println(args...)
-}
-
-// Deprecated: use slog instead
-func Warningf(format string, v ...any) {
-	if lvl > LogLevelWarning {
-		return
-	}
-	logger.Printf(decorate(LogLevelWarning, format), v...)
-}
-
-// Deprecated: use slog instead
-func Info(v ...any) {
-	if lvl > LogLevelInfo {
-		return
-	}
-	args := []any{decorate(LogLevelInfo, "")}
-	args = append(args, v...)
-	logger.Println(args...)
-}
-
-// Deprecated: use slog instead
-func Infof(format string, v ...any) {
-	if lvl > LogLevelInfo {
-		return
-	}
-	logger.Printf(decorate(LogLevelInfo, format), v...)
-}
-
-// Deprecated: use slog instead
-func Error(v ...any) {
-	if lvl > LogLevelError {
-		return
-	}
-	args := []any{decorate(LogLevelError, "")}
-	args = append(args, v...)
-	logger.Println(args...)
-}
-
-// Deprecated: use slog instead
-func Errorf(v ...any) {
-	if lvl > LogLevelError {
-		return
-	}
-	logger.Println(v...)
-}
-
-// Deprecated: use slog instead
 func Fatal(v ...any) {
 	debug.PrintStack()
-	logger.Fatal(v...)
+	log.Fatal(v...)
 }
 
-// Deprecated: use slog instead
 func FatalIf(err error, v ...any) {
 	if err == nil {
 		return
 	}
-	args := []any{decorate(LogLevelError, ""), " ", err}
-	args = append(args, v...)
+	args := append([]any{err}, v...)
 	Fatal(args...)
+}
+
+/******************************************************************************/
+
+type LogHandler struct {
+	handler slog.Handler
+	prefix  string
+}
+
+func (h *LogHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.handler.Enabled(ctx, level)
+}
+
+func (h *LogHandler) Handle(ctx context.Context, r slog.Record) error {
+	return h.handler.Handle(ctx, r)
+}
+
+func (h *LogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h.handler.WithAttrs(attrs)
+}
+
+func (h *LogHandler) WithGroup(name string) slog.Handler {
+	return h.handler.WithGroup(name)
 }
