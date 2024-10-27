45d6a2e7529301407743317edfdd1d08deca2410
Sugat Poudel <taguspoudel+mbp-gpg@gmail.com>
2024-10-27 00:46:03 -0400

    logger: removed slog wrapper

--- logger.go
+++ logger.go
@@ -1,23 +1,9 @@
 package mango
 
 import (
-	"context"
 	"log"
-	"log/slog"
-	"os"
 	"runtime/debug"
 )
-
-func InitLogs(level slog.Level, prefix string) {
-	h := &LogHandler{
-		prefix: prefix,
-		handler: slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-			Level: level,
-		}),
-	}
-	logger := slog.New(h)
-	slog.SetDefault(logger)
-}
 
 func Fatal(v ...any) {
 	debug.PrintStack()
@@ -30,27 +16,4 @@ func FatalIf(err error, v ...any) {
 	}
 	args := append([]any{err}, v...)
 	Fatal(args...)
-}
-
-/******************************************************************************/
-
-type LogHandler struct {
-	handler slog.Handler
-	prefix  string
-}
-
-func (h *LogHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	return h.handler.Enabled(ctx, level)
-}
-
-func (h *LogHandler) Handle(ctx context.Context, r slog.Record) error {
-	return h.handler.Handle(ctx, r)
-}
-
-func (h *LogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return h.handler.WithAttrs(attrs)
-}
-
-func (h *LogHandler) WithGroup(name string) slog.Handler {
-	return h.handler.WithGroup(name)
 }
