7239bf1eb4703ae2db37d00c411824794090bc2a
Sugat Poudel <taguspoudel@gmail.com>
2025-01-19 22:19:19 -0500

    added mango log handler

--- logger.go
+++ logger.go
@@ -1,36 +1,165 @@
 package mango
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"runtime/debug"
+	"strings"
+	"sync"
 )
 
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	White  = "\033[37m"
+)
+
+// Init creates a slog logger instance with a custom LogHandler
+// and sets it as the default logger
 func Init(level slog.Level, application string) {
 	var h slog.Handler
-	h = slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		Level: level,
-	})
-	h = h.WithAttrs([]slog.Attr{
-		slog.String("app", application),
-	})
+	h = &LogHandler{
+		level: level,
+		app:   application,
+		out:   os.Stdout,
+		mu:    &sync.Mutex{},
+	}
 	logger := slog.New(h)
 	slog.SetDefault(logger)
 }
 
-func Fatal(v ...any) {
+// LogHandler is a custom slog handler that outputs to stdout
+type LogHandler struct {
+	app   string
+	level slog.Leveler
+	out   io.Writer
+	attrs []slog.Attr
+
+	// NOTE: since the WithAttrs and WithGroup makes copies of the LogHandler,
+	// we need to ensure that the same mutex instance is used across all instances
+	mu *sync.Mutex
+}
+
+// Enabled only records the log if the record's log level is greater than
+// or equal to the handlers log level
+func (h *LogHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return level >= h.level.Level()
+}
+
+// Handle outputs the log record to stdout. Since this logger is meant to be used for
+// local applications with a small amount of output, it will not log the timestamps.
+func (h *LogHandler) Handle(ctx context.Context, record slog.Record) error {
+	var builder strings.Builder
+
+	_, err := builder.WriteString(fmt.Sprintf("[%s][%s] %s", h.getLevelPrefix(record.Level), h.app, record.Message))
+	if err != nil {
+		return err
+	}
+
+	hasAttrs := len(h.attrs) > 0 || record.NumAttrs() > 0
+	if hasAttrs {
+		_, err := builder.WriteString("\n")
+		if err != nil {
+			return err
+		}
+		for _, attr := range h.attrs {
+			if err := h.appendAttr(&builder, attr); err != nil {
+				return err
+			}
+		}
+		var cbError error
+		record.Attrs(func(attr slog.Attr) bool {
+			if err := h.appendAttr(&builder, attr); err != nil {
+				cbError = err
+				return false
+			}
+			return true
+		})
+		if cbError != nil {
+			return cbError
+		}
+	}
+
+	if !hasAttrs {
+		_, err = builder.WriteString("\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	_, err = h.out.Write([]byte(builder.String()))
+	return err
+}
+
+func (h *LogHandler) appendAttr(builder *strings.Builder, attr slog.Attr) error {
+	// ignore an empty attribute
+	if attr.Equal(slog.Attr{}) {
+		return nil
+	}
+	var value string
+	switch attr.Value.Kind() {
+	case slog.KindString:
+		value = attr.Value.String()
+	case slog.KindTime:
+		value = attr.Value.Time().Format("2006-01-02 15:04:05")
+	default:
+		value = attr.Value.String()
+	}
+	builder.WriteString(fmt.Sprintf("  %s: %s\n", attr.Key, value))
+	return nil
+}
+
+func (h *LogHandler) getLevelPrefix(level slog.Level) string {
+	switch level {
+	case slog.LevelDebug:
+		return fmt.Sprintf("%sDEBUG%s", Blue, Reset)
+	case slog.LevelInfo:
+		return fmt.Sprintf("%sINFO%s", Green, Reset)
+	case slog.LevelWarn:
+		return fmt.Sprintf("%sWARN%s", Yellow, Reset)
+	case slog.LevelError:
+		return fmt.Sprintf("%sERROR%s", Red, Reset)
+	default:
+		return fmt.Sprintf("%sUNKNOWN%s", Purple, Reset)
+	}
+}
+
+func (h *LogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	h.attrs = append(h.attrs, attrs...)
+	return h
+}
+
+// NOTE: groups are not currently supported
+func (h *LogHandler) WithGroup(name string) slog.Handler {
+	return h
+}
+
+/******************************************************************************/
+
+func Fatal(msg string, args ...any) {
 	debug.PrintStack()
-	log.Fatal(v...)
+	slog.Error(msg, args...)
+	os.Exit(1)
 }
 
 func FatalIf(err error, v ...any) {
 	if err == nil {
 		return
 	}
-	args := append([]any{err}, v...)
-	Fatal(args...)
+	Fatal(err.Error(), v...)
 }
+
+/******************************************************************************/
 
 // StackError wraps an error with the stack information
 // where the error occurred. Note that since stack is saved during creation
--- logger_test.go
+++ logger_test.go
@@ -1,10 +1,17 @@
 package mango
 
 import (
+	"bytes"
+	"context"
 	"errors"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"log/slog"
+	"os"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestWrappingStackError(t *testing.T) {
@@ -15,4 +22,64 @@ func TestWrappingStackError(t *testing.T) {
 
 	var stErr *StackError
 	require.Equal(t, true, errors.As(testErr, &stErr))
+}
+
+func TestLogHandler_Enabled(t *testing.T) {
+	ctx := context.TODO()
+
+	tests := []struct {
+		name     string
+		curLevel slog.Level
+		level    slog.Level
+		expected bool
+	}{
+		{
+			name:     "debug < info",
+			curLevel: slog.LevelDebug,
+			level:    slog.LevelInfo,
+			expected: true,
+		},
+		{
+			name:     "info < error",
+			curLevel: slog.LevelError,
+			level:    slog.LevelInfo,
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			lh := &LogHandler{
+				app:   "test-app",
+				level: test.curLevel,
+				out:   os.Stdout,
+				mu:    &sync.Mutex{},
+			}
+
+			actual := lh.Enabled(ctx, test.level)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestLogHandler(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	lh := &LogHandler{
+		app:   "test-app",
+		level: slog.LevelDebug,
+		out:   buf,
+		mu:    &sync.Mutex{},
+	}
+	logger := slog.New(lh).With("foo", "bar")
+
+	ts := time.Date(2025, 1, 19, 21, 49, 1, 0, time.UTC)
+	logger.Warn("warn message", "ts", ts, "count", 2)
+
+	output := buf.String()
+	expected := `[[33mWARN[0m][test-app] warn message
+  foo: bar
+  ts: 2025-01-19 21:49:01
+  count: 2
+`
+	assert.Equal(t, expected, output)
 }
